126914b2db7dbf0ada47d52ba426b20310ea1654
Danilo Soares <65181659+danilosoarex@users.noreply.github.com>
2024-02-13 23:51:56 -0300

    Add files via upload

--- Arrays/dividindo-sala.js
+++ Arrays/dividindo-sala.js
@@ -25,3 +25,10 @@ const sala1 = alunos.slice(0,10);
 const sala2 = alunos.slice(10)
 console.log(sala1);
 console.log(sala2);
+
+const animais = [1,2,3,4];
+console.log(animais);
+animais.splice(1,0,5)
+console.log(animais);
+animais.splice(3,2,6)
+console.log(animais);
---
+++ Arrays/juntando-salas.js
@@ -0,0 +1,5 @@
+const salaJS = ['Cristiano', 'Messi', 'Ibrahimovic'];
+const salaPy = ['Ronaldo','Dentinho','Jorge Henrique'];
+
+const salas = salaPy.concat(salaJS);
+console.log(salas);
