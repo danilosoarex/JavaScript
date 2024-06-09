ab2b9d3fd44bf8811203d557d286d15daeb0f066
Danilo Soares <danilocs.network@gmail.com>
2024-06-08 22:40:33 -0300

    Códigos do Curso de Javascript: Conhecendo o Browser e Padrões de Projeto

--- Padrões de Projeto MVC/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Padrões de Projeto MVC/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,34 +6,39 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+        
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
 
     }
  
     adiciona(event) {
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
 
-       //let data = new Date(...
-       //    this._inputData.value
-       //    .split('-')
-       //    .map(function(item, indice) {
-       //        return item - item % 2;
-       //    })
-       // );
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+        
+        this._limpaFormulario();       
+    }
 
-       //ArrowFunction
-       let data = new Date(
-        ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
+    _criaNegociacao (){
+        return new Negociacao (
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
 
-        let negociacao = new Negociacao (
-               data,
-               this._inputQuantidade.value, 
-               this._inputValor.value
-        );
-
-        console.log(negociacao);
-
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 } 
